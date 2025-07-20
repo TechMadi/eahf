7e80223b8000d57ce56a4f9b8a8358cd516a75c8
TechMadi <winnieamandela@gmail.com>
2025-07-20 15:03:55 +0300

    deploy : setting up  hosting

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+declare global {
+	interface Window {
+		HSStaticMethods: {
+			autoInit: () => void;
+		};
+	}
+}
+
+import { Component, inject, OnInit } from "@angular/core";
+import { NavigationEnd, Router, RouterOutlet, Event } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+	selector: "app-root",
+	imports: [RouterOutlet],
+	templateUrl: "./app.html",
+	styleUrl: "./app.scss",
 })
-export class App {
-  protected title = 'eaf-website';
+export class App implements OnInit {
+	protected title = "eaf-website";
+	router = inject(Router);
+
+	ngOnInit() {
+		this.router.events.subscribe((event: Event) => {
+			if (event instanceof NavigationEnd) {
+				setTimeout(() => window.HSStaticMethods.autoInit(), 100);
+			}
+		});
+	}
 }
