e8579a085c471fd4a9afdda42acb4a2fa2880571
TechMadi <winnieamandela@gmail.com>
2025-07-21 10:57:04 +0300

    page : Coming soon page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { ComingSoon } from "../shared/pages/coming-soon/coming-soon";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: "",
+		component: ComingSoon,
+		pathMatch: "full",
+	},
+	{
+		path: "**",
+		component: ComingSoon,
+		pathMatch: "full",
+	},
+];
