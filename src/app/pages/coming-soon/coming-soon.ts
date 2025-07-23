f371dd5f78a5d76578727c2f8035d8a1257f21dc
TechMadi <winnieamandela@gmail.com>
2025-07-23 03:44:29 +0300

    bug  :  navigation

--- src/app/pages/coming-soon/coming-soon.ts
+++ src/app/pages/coming-soon/coming-soon.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Navbar } from "../../../shared/components/layout/navbar/navbar";
+import { Container } from "../../../shared/components/container/container";
 
 @Component({
-  selector: 'app-coming-soon',
-  imports: [],
-  templateUrl: './coming-soon.html',
-  styleUrl: './coming-soon.scss'
+	selector: "app-coming-soon",
+	imports: [Navbar, Container],
+	templateUrl: "./coming-soon.html",
+	styleUrl: "./coming-soon.scss",
 })
-export class ComingSoon {
-
-}
+export class ComingSoon {}
